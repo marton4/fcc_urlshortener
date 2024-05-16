54077737916e814a87a1f1620e066103731d68b0
Mráz Márton Damján <mraz.marton@gmail.com>
2024-05-16 12:06:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,11 +27,6 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
-
 // my code 
 
 let originalUrls = [];
@@ -39,7 +34,11 @@ let shortUrls = [];
 
 app.post('/api/shorturl', (req, res) => {
   const url = req.body.url;
-  
+
+  if(!url.includes(`https://`) && !url.includes(`http://`)) {
+    return res.json({ error: 'invalid url' })
+  };
+
   if (!originalUrls.includes(url)) {
     originalUrls.push(url);
     shortUrls.push(shortUrls.length);
@@ -48,16 +47,24 @@ app.post('/api/shorturl', (req, res) => {
       original_url: url,
       short_url: shortUrls.length - 1
     })
+  };
 
   return res.json({
-    
-  })
-  };
-  
-  res.json({original_url: originalUrls[0]},{short_url: shortUrls[0]});
-
+    original_url: url,
+    short_url: shortUrls[originalUrls.indexOf(url)]
   });
+});
 
+app.get('/api/shorturl/:shortUrl', (req, res) => {
+  const shortUrl = req.params.shortUrl;
 
+  if (shortUrl >= shortUrls.length) {
+    return res.json({ error: 'invalid url' })
+  };
 
+  return res.redirect(originalUrls[shortUrl]);
+});
 
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+});
