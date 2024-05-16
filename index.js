3974c0d30c5e521693244cc617864e7c0772a193
Mráz Márton Damján <mraz.marton@gmail.com>
2024-05-16 12:21:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/index.html');
 });
 
 // Your first API endpoint
